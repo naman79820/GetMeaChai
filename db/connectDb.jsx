00d4c9fe0b70fed4ced1aab28393867650be28a3
“naman79820” <naman79820@gmail.com>
2024-08-02 17:19:46 +0530

    some more tweaks (2.0)

--- db/connectDb.jsx
+++ db/connectDb.jsx
@@ -4,9 +4,9 @@ const connectDB = async () => {
     
   try {
     
-    const conn = await mongoose.connect('mongodb+srv://naman79820:c53WIJxaKEE0fT9O@cluster0.yubogm4.mongodb.net/Chai', {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI 
     
-    });
+    );
    
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
